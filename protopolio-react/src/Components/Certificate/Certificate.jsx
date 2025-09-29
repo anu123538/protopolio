5272453e586cdc7f31283ad3b9a453842275266e
Anuththara Imanshi <anuththaraimanshi842@gmail.com>
2025-09-29 22:27:50 +0100

    update

--- protopolio-react/src/Components/Certificate/Certificate.jsx
+++ protopolio-react/src/Components/Certificate/Certificate.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import './Certificate.css';
 import uiCertificate from '../../assets/ui.jpg';
 import moCertificate from '../../assets/mo.png';
+import tcertificate from '../../assets/t.png';
+import cicertificate from '../../assets/ci.jpeg';
+import hkcertificate from '../../assets/hk.jpeg';
 
 
 const certificates = [
@@ -10,7 +13,7 @@ const certificates = [
     title: "HACKBLAST COMPETITION",
     issuer: "IEEE",
     year: "2024",
-    image: "" ,  // replace with your image paths
+    image: hkcertificate ,  // replace with your image paths
   },
   {
   id: 2,
@@ -24,17 +27,25 @@ const certificates = [
     title: "Smart School Selector",
     issuer: "CINEC INTERNATIONAL RESEARCH SYMPOSIUM",
     year: "2024",
-    image: "",
+    image: cicertificate,
 },
+{
+  id: 4,
+    title: "Introduction of Testing and JUNIT ,Cucumber",
+    issuer: "Skillup",
+    year: "2025",
+    image:tcertificate,
+},
+
   {
-    id: 4,
+    id: 5,
     title: "Python for Beginners",
     issuer: "University of Moratuwa",
     year: "2023",
     image: moCertificate,
   },
   {
-    id: 5,
+    id: 6,
     title: "UI/UX  For Beginners",
     issuer: "Great Learning",
     year: "2024",
